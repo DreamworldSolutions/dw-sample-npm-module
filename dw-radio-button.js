c46c8fc38453050ec84113dcc0c8e8f27c556ca5
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2020-09-28 18:20:55 +0530

    fix: click on radio button refresh hold page in safari.

--- dw-radio-button.js
+++ dw-radio-button.js
@@ -62,6 +62,16 @@ export class DWRadioButton extends DwFormElement(Radio) {
     }
   }
 
+  /**
+   * Web view crashing on iOS 14 b5 and Safari Technology Preview when interacting with mwc elements
+   * for Details see this[https://github.com/material-components/material-components-web-components/issues/1720]
+   * @override
+   */
+  createRenderRoot() {
+    // don't set delegatesFocus: true due to https://bugs.webkit.org/show_bug.cgi?id=215732
+    return this.attachShadow({mode: 'open'});
+  }
+
   render() {
     let superElementRender = super.render();
     return html `
